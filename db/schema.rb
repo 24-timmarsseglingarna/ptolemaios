90c2d0eb31cbb2cd5ac68f63ec43f18b9d40a10b
Stefan Pettersson <stefan.pettersson@lumano.se>
2016-12-19 15:57:05 +0100

    Go postgres!

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161218130533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.integer  "logbook_id"
     t.datetime "time"
